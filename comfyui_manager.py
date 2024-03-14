aaa62e473121f2b9e10c0b4ead3341430eb3b36d
Hanke Chen <i@kokecacao.me>
2024-03-13 19:04:22 -0700

    :sparkles: properly load comfy module and folder_paths file

--- comfyui_manager.py
+++ comfyui_manager.py
@@ -3,40 +3,20 @@ from backend.loader.decorator import KatzukiNode
 from backend.nodes.builtin import BaseNode
 from typing import Any
 
-current_dir = os.path.abspath(os.path.dirname(__file__))
-target_dir = os.path.abspath(os.path.join(current_dir, "../../"))
-target_folder_paths_path = os.path.join(target_dir, "folder_paths.py")
-target_gitignore_path = os.path.join(target_dir, ".gitignore")
-installed_plugins_cache_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "installed_plugins_cache.json")
-
-# Create proper gitignore
-ignores = [".gitignore", "folder_paths.py"]
-current_ignore = []
-if os.path.exists(target_gitignore_path):
-    with open(target_gitignore_path, "r") as f:
-        current_ignore = [line.strip() for line in f.readlines()]
-
-with open(target_gitignore_path, "a") as f:
-    for ignore in ignores:
-        # if gitignore don't already contain ignore, write it to gitignore
-        if ignore not in current_ignore:
-            f.write(f"{ignore}\n")
-
-# Create a file at "./src/folder_paths.py" if not exist
-# KatUI will load this file first before main.py and before any other file in subdirectories
-try:
-    import folder_paths # type: ignore
-except ImportError:
-    # if there is no file at "../../folder_paths.py", create it
-    print(f"Creating folder_paths.py at {target_folder_paths_path}, since it does not exist.")
-
-    with open(target_folder_paths_path, "w") as f:
-        f.write("from nodes.ComfyUIManager._folder_paths import *")
-    import folder_paths # type: ignore
-
 # Dynamically load comfy as a module
 import sys
 import importlib.util
+
+
+def load_module(module_name, module_path):
+    spec = importlib.util.spec_from_file_location(module_name, module_path)
+    if spec is not None and spec.loader is not None:
+        module = importlib.util.module_from_spec(spec)
+        sys.modules[module_name] = module
+        spec.loader.exec_module(module)
+        return module
+    else:
+        raise ImportError(f"Could not load module {module_name} from {module_path}")
 
 
 def load_package(package_name, package_path):
@@ -50,9 +30,11 @@ def load_package(package_name, package_path):
         raise ImportError(f"Could not load package {package_name} from {package_path}")
 
 
-# Example usage
-package_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "_comfy")
-package = load_package('comfy', package_path)
+# Add _folder_paths to sys.modules as folder_paths module
+load_module('folder_paths', os.path.join(os.path.dirname(os.path.realpath(__file__)), "_folder_paths.py"))
+
+# Add comfy to sys.modules as comfy package
+load_package('comfy', os.path.join(os.path.dirname(os.path.realpath(__file__)), "comfy"))
 
 
 class ComfyUIManager(BaseNode):
